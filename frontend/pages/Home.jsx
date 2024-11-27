d92970940778ff72e2d931d3e001a53aac106b22
Dimalsha Praveen <dimalshapraveen2001@gmail.com>
2024-11-27 11:08:53 +0530

    Update Home.jsx

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -14,8 +14,8 @@ function Portfolio() {
 
   const handleDownloadCV = () => {
     const link = document.createElement('a');
-    link.href = "https://github.com/SLDima2001/My-PortFolio/blob/main/frontend/Dimalsha%20Resume.pdf"; // File in the public folder
-    link.download = "https://github.com/SLDima2001/My-PortFolio/blob/main/frontend/Dimalsha%20Resume.pdf";
+    link.href = "frontend/public/Dimalsha Resume.pdf"; // File in the public folder
+    link.download = "frontend/public/Dimalsha Resume.pdf";
     link.click();
   };
 
